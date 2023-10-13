bfcea429187298eeb9a7ec71e56256435638ebbf
dperezv-com <dperezv-com@users.noreply.github.com>
2023-10-13 18:39:13 +0000

    deploy: dperezv-com/app-qr-menu@bd121e719b0e46f29332ed1004cbe942ceeb6407

--- app-qr-menu/flutter_service_worker.js
+++ app-qr-menu/flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "96ae916cd2d1b7320fff853ee
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ce2793ac8541679b611e3ffe3538764a",
-"/": "ce2793ac8541679b611e3ffe3538764a",
-"main.dart.js": "8d4c09adc0cfa27e9f92eae8969a354c",
+"index.html": "17e4ea6f50b88dd8bc8d9f46e12fa446",
+"/": "17e4ea6f50b88dd8bc8d9f46e12fa446",
+"main.dart.js": "55719a3c5389b3212e521f875d41eec5",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "f5dba90805b98f917d3f773a98a17c9b"};
